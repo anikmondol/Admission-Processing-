406a5b28898cf43b7407d11ec62b56b7f0e2789f
anikmondol <anikmondol558363@gmil.com>
2024-05-28 15:48:06 +0600

    set and get data for localstorage

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,68 @@
+import { useState } from "react";
+import {useNavigate } from "react-router-dom";
 
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [input, setInput] = useState({
+        email: "",
+        password: "",
+
+    });
+
+
+
+    // to store value in localStorage
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const loggedUser = JSON.parse(localStorage.getItem("user"));
+
+        if(input.email === loggedUser.email && input.password === loggedUser.password){
+            {
+                navigate('/')
+            }
+        }else{
+            alert("wrong Email or Password")
+        }
+        
+    }
+
+
     return (
-        <div>
-            Login
+        <div className="hero min-h-screen bg_img">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 px-10">
+                    <h1 className='mt-5 mx-auto text- xl font-bold'>CREATE AN ACCOUNT</h1>
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name="email"
+                                value={input.email}
+                                onChange={(e) => setInput({ ...input, [e.target.name]: e.target.value })} type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name="password"
+                                value={input.password}
+                                onChange={(e) => setInput({ ...input, [e.target.name]: e.target.value })} type="password" placeholder="password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary w-4/6 mx-auto">Login</button>
+                        </div>
+                    </form>
+                    <div className='my-5 flex'>
+                        <p>Have already an account?</p>
+                        <a href="#" className='underline font-medium'>Register here</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,42 +1,68 @@
+import { useState } from 'react';
 import '../Css/Register.css'
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const navigate = useNavigate() 
+
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        password: "",
+
+    });
+
+
+    // to store value in localStorage
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        localStorage.setItem("user", JSON.stringify(input));
+        navigate("/login")
+    }
+
+
+
     return (
         <div className="hero min-h-screen bg_img">
             <div className="hero-content flex-col lg:flex-row-reverse">
 
-
-                
-
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 px-10">
-                <h1 className='mt-5 mx-auto text- xl font-bold'>CREATE AN ACCOUNT</h1>
-                    <form className="card-body">
+                    <h1 className='mt-5 mx-auto text- xl font-bold'>CREATE AN ACCOUNT</h1>
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="Name" className="input input-bordered" required />
+                            <input name="name"
+                            value={input.name} 
+                            onChange={(e) => setInput({...input, [e.target.name] : e.target.value})} type="text" placeholder="Name" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input name="email"
+                            value={input.email} 
+                            onChange={(e) => setInput({...input, [e.target.name] : e.target.value})} type="email" placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input name="password"
+                            value={input.password} 
+                            onChange={(e) => setInput({...input, [e.target.name] : e.target.value})} type="password" placeholder="password" className="input input-bordered" required />
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary w-4/6 mx-auto">Login</button>
+                            <button className="btn btn-primary w-4/6 mx-auto">Register</button>
                         </div>
                     </form>
                     <div className='my-5 flex'>
                         <p>Have already an account?</p>
                         <a href="#" className='underline font-medium'>Login here</a>
-                    </div>    
+                    </div>
                 </div>
             </div>
         </div>
